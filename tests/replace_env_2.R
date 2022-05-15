bcec790f67debd05e414e5fd0a2a13ce3c6461f9
Henrik Bengtsson <hb@aroma-project.org>
2022-05-14 21:08:25 -0700

    Add replace_env() tests for function within local environments

---
+++ tests/replace_env_2.R
@@ -0,0 +1,96 @@
+library(environments)
+
+message("*** Setup")
+
+tf <- tempfile(fileext = ".rds")
+tf2 <- tempfile(fileext = ".rds")
+tf3 <- tempfile(fileext = ".rds")
+
+
+message("*** Exporting function part of a local environment")
+
+local({
+  cargo <- rnorm(1e6)
+  a <- 2
+  f <- local({
+    pi <- 3.14
+    function() {
+      n <- 4
+      a * pi / n
+    }
+  })
+  stopifnot(
+    identical(environment(f)$pi, 3.14),
+    identical(parent.env(environment(f))$a, 2),
+    length(parent.env(environment(f))$cargo) == 1e6
+  )
+  saveRDS(f, file = tf)
+  truth <<- f()
+  print(truth)
+
+  ## Wrap f() and 'a' up in a local environment
+  new <- new.env()
+  new$a <- a
+  parent.env(environment(f)) <- new
+  stopifnot(
+    identical(environment(f)$pi, 3.14),
+    identical(parent.env(environment(f))$a, 2),
+    is.null(parent.env(environment(f))$cargo)
+  )
+  saveRDS(f, file = tf2)
+
+  new <- as.environment(list(a = a))
+  replace_env(environment(f), search = locate_object(f)$envir, replace = new)
+  stopifnot(
+    identical(environment(f)$pi, 3.14),
+    identical(parent.env(environment(f))$a, 2),
+    is.null(parent.env(environment(f))$cargo)
+  )
+  saveRDS(f, file = tf3)
+})  
+
+
+message("*** Verify exported functions")
+
+## Since we everything is in a local environment when f() is serialized,
+## all of the local environment is brought along
+f <- readRDS(tf)
+stopifnot(
+  identical(environment(f)$pi, 3.14),
+  identical(parent.env(environment(f))$a, 2),
+  length(parent.env(environment(f))$cargo) == 1e6
+)
+res <- f()
+print(res)
+stopifnot(identical(res, truth))
+rm(list = c("f"))
+
+
+## However, the serialized, modified versions behave as expected
+f <- readRDS(tf2)
+stopifnot(
+  identical(environment(f)$pi, 3.14),
+  identical(parent.env(environment(f))$a, 2),
+  is.null(parent.env(environment(f))$cargo)
+)
+res <- f()
+print(res)
+stopifnot(identical(res, truth))
+rm(list = c("f"))
+
+
+f <- readRDS(tf3)
+stopifnot(
+  identical(environment(f)$pi, 3.14),
+  identical(parent.env(environment(f))$a, 2),
+  is.null(parent.env(environment(f))$cargo)
+)
+res <- f()
+print(res)
+stopifnot(identical(res, truth))
+rm(list = c("f"))
+
+
+message("*** Cleanup")
+invisible(file.remove(c(tf, tf2, tf3)))
+

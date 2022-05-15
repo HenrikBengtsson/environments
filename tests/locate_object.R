87668b328115d41fddb62b4084884a1bab36f7d0
Henrik Bengtsson <hb@aroma-project.org>
2022-05-14 22:45:08 -0700

    TESTS: Ignore '.Last.value' results introduced by R CMD check

---
+++ tests/locate_object.R
@@ -0,0 +1,94 @@
+library(environments)
+library(stats)
+
+message("*** Setup")
+
+## WORKAROUND: R CMD check adds '.Last.value' when running tests
+prune_check_env <- function(envir = parent.frame()) {
+  if (exists(".Last.value", envir = envir, inherits = FALSE)) {
+    rm(".Last.value", envir = envir)
+  }
+}
+
+message("*** Locate stats::rnorm()")
+res <- locate_object(stats::rnorm)
+str(res)
+stopifnot(
+  is.list(res),
+  res$name == "rnorm",
+  identical(res$envir, as.environment("package:stats"))
+)
+
+
+message("*** Locate local copy of stats::rnorm()")
+my_rnorm <- stats::rnorm
+prune_check_env()
+res <- locate_object(my_rnorm)
+str(res)
+stopifnot(
+  is.list(res),
+  res$name == "my_rnorm",
+  identical(res$envir, environment())
+)
+
+
+message("*** Locate stats::rnorm() from parent environment")
+my_rnorm <- stats::rnorm
+prune_check_env()
+res <- locate_object(my_rnorm, from = parent.env(environment()))
+str(res)
+stopifnot(
+  is.list(res),
+  res$name == "rnorm",
+  identical(res$envir, as.environment("package:stats"))
+)
+
+
+message("*** Locate stats::rnorm() despite local copy")
+my_rnorm <- stats::rnorm
+prune_check_env()
+res <- locate_object(my_rnorm, first = FALSE)
+str(res)
+stopifnot(
+  is.list(res),
+  res$name == "rnorm",
+  identical(res$envir, as.environment("package:stats"))
+)
+
+
+message("*** Locate local-environment copy of stats::rnorm()")
+local({
+  my_rnorm <- stats::rnorm
+  res <- locate_object(my_rnorm)
+  str(res)
+  stopifnot(
+    is.list(res),
+    res$name == "my_rnorm",
+    identical(res$envir, environment())
+  )
+})
+
+
+message("*** Locate from within function")
+
+my_find <- function(object, first = TRUE) {
+  locate_object(object, first = first)
+}
+
+my_fcn <- local({
+  g <- stats::rnorm  ## this is never found
+  function(f = NULL) {
+    if (is.null(f)) {
+      f <- g  ## this is found first
+      my_find(f)
+    } else {
+      my_find(f, first = FALSE) # skip argument 'f'
+    }
+  }
+})
+
+res <- my_fcn()
+str(res)
+
+res <- my_fcn(my_rnorm)
+str(res)

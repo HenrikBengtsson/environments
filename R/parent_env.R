18eaf46ca62fcf8be742c766e5f992a02d78d971
Henrik Bengtsson <hb@aroma-project.org>
2022-05-14 21:17:29 -0700

    Add parent_env()

---
+++ R/parent_env.R
@@ -0,0 +1,44 @@
+#' Get ancestral environments of an environment
+#'
+#' @param envir An \code{\link[base:environment]{environment}}.
+#'
+#' @param n The generation of parent environment to get.
+#' If `n = 0`, then `envir` is returned.
+#' If `n = 1`, then `parent.env(envir)` is returned.
+#' If `n = 2`, then `parent.env(parent.env(envir))` is returned,
+#' and so on.
+#'
+#' @return
+#' An \code{\link[base:environment]{environment}}.
+#'
+#' @examples
+#' ns <- getNamespace("stats")
+#' parent_env(ns, n = 0)
+#' parent_env(ns, n = 1)
+#' parent_env(ns, n = 2)
+#' parent_env(ns, n = 3)
+#' parent_env(ns, n = Inf)  ## always emptyenv()
+#'
+#' f <- local({
+#'   a <- 42
+#'   local({
+#'     pi <- 3.14
+#'     function() pi * a
+#'   })
+#' })
+#' parent_env(environment(f), n = 1)
+#' parent_env(environment(f), n = 2)
+#' parent_env(environment(f), n = 3)
+#'
+#' @export
+parent_env <- function(envir, n = 1L) {
+  stopifnot(inherits(envir, "environment"))
+  stopifnot(is.numeric(n), length(n) == 1L, !is.na(n), n >= 0L)
+
+  while (n > 0L && !identical(envir, emptyenv())) {
+    envir <- parent.env(envir)
+    n <- n - 1L
+  }
+  
+  envir
+}

a647c62f135e3305d5c6a713c140660414fab7aa
Henrik Bengtsson <hb@aroma-project.org>
2022-05-17 23:02:40 -0700

    More tests - showing that we have to be careful what we prune

--- tests/prune_fcn_5.R
+++ tests/prune_fcn_5.R
@@ -19,3 +19,11 @@ stopifnot(identical(y, truth))
 
 prune_undo <- attr(my_rnorm, "prune_undo")
 prune_undo()
+
+
+message("*** Prune stats::rnorm (2)")
+my_rnorm <- prune_fcn(stats::rnorm, search = environment(stats::rnorm))
+res <- tryCatch(y <- my_rnorm(10), error = identity)
+stopifnot(inherits(res, "error"))
+prune_undo <- attr(my_rnorm, "prune_undo")
+prune_undo()

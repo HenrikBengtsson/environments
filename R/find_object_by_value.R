ceabb7a9607ffb1704e87f803e15c3f396775b69
Henrik Bengtsson <hb@aroma-project.org>
2022-10-03 23:30:03 -0700

    find_object_by_value(): rename argument 'object' to 'value'

--- R/find_object_by_value.R
+++ R/find_object_by_value.R
@@ -2,7 +2,7 @@
 #'
 #' @inheritParams find_object_by_name
 #'
-#' @param object The R object whose location should be identified.
+#' @param value The R object whose location should be identified.
 #'
 #' @param which If `"first"` or `"last"`, then the first or the last
 #' occurance of `object` among the parent frames is identified and returned.
@@ -21,7 +21,7 @@
 #' @example incl/find_object_by_value_1.R
 #'
 #' @export
-find_object_by_value <- function(object, from = parent.frame(), until = emptyenv(), which = c("first", "last", "all")) {
+find_object_by_value <- function(value, from = parent.frame(), until = emptyenv(), which = c("first", "last", "all")) {
   if (inherits(from, "environment")) {
     envir <- from
   } else {
@@ -46,7 +46,7 @@ find_object_by_value <- function(object, from = parent.frame(), until = emptyenv
   
   which <- match.arg(which)
   
-  mode <- mode(object) ## scan for objects of this mode
+  mode <- mode(value) ## scan for objects of this mode
 
   ## WORKAROUND: R CMD check will add mockup 'F' and 'T' objects
   ## to catch misuse of 'F' instead of 'FALSE' and 'T' and 'TRUE'
@@ -72,11 +72,11 @@ find_object_by_value <- function(object, from = parent.frame(), until = emptyenv
       names <- setdiff(names, ".Last.value")
     }
     for (name in names) {
-      ## ... consider only those with the same mode as 'object' ...
+      ## ... consider only those with the same mode as 'value' ...
       if (exists(name, mode = mode, envir = envir, inherits = FALSE)) {
         tmp <- get(name, mode = mode, envir = envir, inherits = FALSE)
         ## ... are the identical?
-        if (identical(tmp, object, ignore.environment = FALSE, ignore.bytecode = FALSE, ignore.srcref = FALSE)) {
+        if (identical(tmp, value, ignore.environment = FALSE, ignore.bytecode = FALSE, ignore.srcref = FALSE)) {
           res_name <- list(name = name, envir = envir)
           if (which == "first") return(res_name)
           res <- c(res, list(res_name))

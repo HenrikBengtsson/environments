0248d662dd81a6e5bc1f246d834f6f6ebca2c01e
Henrik Bengtsson <hb@aroma-project.org>
2022-10-07 08:42:35 -0700

    Add environment_of() [#14]

---
+++ R/environment_of.R
@@ -0,0 +1,42 @@
+#' Get the environment of an object
+#'
+#' @param envir An R object.
+#'
+#' @return A non-empty character string.
+#'
+#' @details
+#' Returns an \code{\link[base:environmentName]{environmentName(envir)}},
+#' if `envir` is an \code{\link[base:environment]{environment}},
+#' a function, or a formula.  In all other cases, NULL is returned.
+#'
+#' @examples
+#' ## An environment
+#' environment_of(globalenv())    ## <environment: R_GlobalEnv>
+#'
+#' ## A primitive function (always in the 'base' package)
+#' environment_of(abs)            ## <environment: base>
+#'
+#' ## A package function
+#' environment_of(mean)           ## <environment: base>
+#'
+#' ## Another package function
+#' environment_of(stats::median)  ## namespace:stats
+#'
+#' ## A formula
+#' environment_of(y ~ x)          ## <environment: R_GlobalEnv>
+#'
+#' ## A non-function object
+#' environment_of(pi)             ## NULL
+#' 
+#' @export
+environment_of <- function(envir) {
+  if (inherits(envir, "environment")) return(envir)
+  
+  e <- environment(envir)
+  if (!is.null(e)) return(e)
+     
+  ## Special case: primitive functions are all in the 'base' package
+  if (is.primitive(envir)) return(baseenv())
+
+  NULL
+}
